80dab8664ff57fb645738ca6fe1b82daf7c9f096
Muhammad Dhiya Ulhaq <uhqdev@gmail.com>
2021-09-04 16:47:10 +0800

    first commit

---
+++ src/pages/LandingPage/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import Button  from 'atoms/button';
+export default class index extends Component {
+    render(){
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
